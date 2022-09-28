1e255dbae01964ec128f6acd9419702940117d10
wahidnoory <wahidnoory1@gmail.com>
2022-09-28 18:50:44 -0400

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,6 +4,7 @@ public class class03 {
 		public static void main(String[] arggs) {
 			
 			System.out.println("Hey this is Git class");
+			System.out.println("Hey this is a change in my project");
 			
 
 	}
